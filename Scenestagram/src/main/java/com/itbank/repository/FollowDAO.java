68559577090eb6280d9e0da32884351a39115ca3
kdhwon1@gmail.com <115617509+svdjcuwg4638@users.noreply.github.com>
2023-02-11 15:02:13 +0900

    commit

---
+++ Scenestagram/src/main/java/com/itbank/repository/FollowDAO.java
@@ -0,0 +1,17 @@
+package com.itbank.repository;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FollowDAO {
+
+	int addFollow(HashMap<String, Integer> idxhash);
+
+	int unFollow(HashMap<String, Integer> idxhash);
+
+	int findFollow(HashMap<String, Integer> idxHash);
+	
+	
+}
---
+++ Scenestagram/src/main/java/com/itbank/repository/UsersDAO.java
@@ -0,0 +1,33 @@
+﻿package com.itbank.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.itbank.model.UsersDTO;
+
+@Repository
+public interface UsersDAO {
+
+	int insert(UsersDTO dto);	// 회원가입
+
+	UsersDTO login(UsersDTO dto);	// 로그인 체크
+	
+	int updateUserStatus(int idx);
+
+	int infoUpdate(UsersDTO dto);
+
+	int updateUserProfile(UsersDTO dto);
+
+	UsersDTO selectOne(int idx);
+
+	int idCheck(String id);
+
+	List<UsersDTO> getList();
+
+	String findId(UsersDTO dto);
+
+	List<UsersDTO> usersSearch(String search);
+
+
+}
